9a1f528d64d684468b6f5bb7eb63cf3e3e436bef
Rasmus <rasmushpedersen@msn.com>
2023-10-28 21:18:23 +0200

    second commit

--- components/providers/modal-provider.tsx
+++ components/providers/modal-provider.tsx
@@ -2,7 +2,7 @@
 
 import { useEffect, useState } from "react";
 
-import { EditServerModal } from "@/components/modals/edit-modal-server";
+import { EditServerModal } from "@/components/modals/edit-server-modal";
 import { InviteModal } from "@/components/modals/invite-modal";
 import { CreateServerModal } from "@/components/modals/create-server-modal";
 import { MembersModal } from "@/components/modals/members-modal";
@@ -11,29 +11,33 @@ import { LeaveServerModal } from "@/components/modals/leave-server-modal";
 import { DeleteServerModal } from "@/components/modals/delete-server-modal";
 import { DeleteChannelModal } from "@/components/modals/delete-channel-modal";
 import { EditChannelModal } from "@/components/modals/edit-channel-modal";
+import { MessageFileModal } from "@/components/modals/message-file-modal";
+import { DeleteMessageModal } from "@/components/modals/delete-message-modal";
 
 export const ModalProvider = () => {
-    const [isMounted, setIsMounted] = useState(false);
+  const [isMounted, setIsMounted] = useState(false);
 
-    useEffect(() => {
-        setIsMounted(true);
-    }, []);
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
 
-    if (!isMounted) {
-        return null;
-    }
+  if (!isMounted) {
+    return null;
+  }
 
-    return (
-        <>
-            <CreateServerModal />
-            <InviteModal />
-            <EditServerModal />
-            <MembersModal />
-            <CreateChannelModal />
-            <LeaveServerModal />
-            <DeleteServerModal />
-            <DeleteChannelModal />
-            <EditChannelModal />
-        </>
-    )
+  return (
+    <>
+      <CreateServerModal />
+      <InviteModal />
+      <EditServerModal />
+      <MembersModal />
+      <CreateChannelModal />
+      <LeaveServerModal />
+      <DeleteServerModal />
+      <DeleteChannelModal />
+      <EditChannelModal />
+      <MessageFileModal />
+      <DeleteMessageModal />
+    </>
+  )
 }
